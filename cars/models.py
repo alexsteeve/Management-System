f2a01a6c5876aaabbba658144d02672d8e3de7c0
alexsteeve <alexsteeve@gmail.com>
2023-03-21 13:54:07 -0400

    criacao da tabela de parts

--- cars/models.py
+++ cars/models.py
@@ -27,3 +27,30 @@ class Car(models.Model):
     transport = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     dismantle = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     weight = models.IntegerField()
+
+    def __str__(self):
+        return self.car
+
+class Part(models.Model):
+    VIN = models.ForeignKey(Car, default=1, on_delete=models.SET_DEFAULT)
+    STATUS_PART_CHOICES = [
+        ('in_the_car', 'IN THE CAR'),
+        ('in_stock', 'IN STOCK'),
+        ('sold', 'SOLD'),
+    ]
+    TYPE_CHOICES = [
+        ('ac_compressor', 'A/C COMPRESSOR'),
+        ('abs_module', 'ABS MODULE'),
+        ('airbag', 'AIRBAG'),
+        ('alternator', 'ALTERNATOR'),
+        ('battery', 'BATTERY'),
+        ('body_control_module', 'BODY CONTROL MODULE'),
+        ('booster', 'BOOSTER'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_PART_CHOICES)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    max_value = models.DecimalField(max_digits=10, decimal_places=2)
+    value_paid = models.DecimalField(max_digits=10,decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+       return self.part
